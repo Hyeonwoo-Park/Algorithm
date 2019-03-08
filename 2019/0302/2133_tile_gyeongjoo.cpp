dd120ee936f961738ead9103852af9231acd622f
gyeongjoo1 <44029908+gyeongjoo1@users.noreply.github.com>
2019-03-09 02:21:04 +0900

    Add files via upload

---
+++ 2019/0302/2133_tile_gyeongjoo.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+long long d[31];
+using namespace std;
+
+int main()
+{
+	int N; //N must be an even number.
+	cin >> N;
+	d[0] = 1;
+	
+	for (int i = 1; i <= N; i++) {
+		d[i] = 3 * d[i - 2];
+		for (int k = 2; i - 2 * k >= 0; k++) {
+			d[i] += 2 * d[i - 2 * k];
+		}
+	}
+	
+	cout << "ans= " << d[N] << '\n';
+	return 0;
+}
+
