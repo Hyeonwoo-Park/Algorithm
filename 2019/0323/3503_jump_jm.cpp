d4a7080f9234f9ffa2596c1e5b02e7b20812b2dc
jm1224k <38150578+jm1224k@users.noreply.github.com>
2019-03-27 14:54:08 +0900

    Update 3503_jump_jm.cpp

--- 2019/0323/3503_jump_jm.cpp
+++ 2019/0323/3503_jump_jm.cpp
@@ -1,46 +1,64 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include<iostream>
+#include<deque>
+#include<vector>
+#include<algorithm>
 using namespace std;
-
-int main() {
-
-	int t;
-	cin >> t;
-
-	for (int test = 1; test <= t; test++)
-	{
-		int n;
-		vector <int> v;
-		int minimum = 0;
-		cin >> n;
-
-		for (int i = 0; i < n; i++)
-		{
-			int x;
-			cin >> x;
-			v.push_back(x);
-		}
-		sort(v.begin(), v.end());
-
-		int i = 0;
-		if (v[i + 2] - v[i] > minimum)
-			minimum = v[i + 2] - v[i];
-
-		i = n - 1;
-
-		if (v[i] - v[i - 2] > minimum)
-			minimum = v[i] - v[i - 2];
-
-		for (int i = 1; i < n - 1; i += 2)
-		{
-			if (v[i] - v[i - 1] > minimum)
-				minimum = v[i] - v[i - 1];
-			else if (v[i + 1] - v[i] > minimum)
-				minimum = v[i + 1] - v[i];
-		}
-
-		cout << '#' << test << ' ' << minimum << '\n';
-	}
-	return 0;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    for (int test = 1; test <= t; test++)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        deque<int> dq;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+ 
+        sort(v.begin(), v.end());
+ 
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                dq.push_front(v[i]);
+            }
+            else
+            {
+                if (i % 2 == 1)
+                {
+                    dq.push_front(v[i]);
+                }
+                else
+                {
+                    dq.push_back(v[i]);
+                }
+            }
+        }
+ 
+        int minimum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+            {
+                if(minimum<abs(dq[i] - dq[0]))
+                minimum = abs(dq[i] - dq[0]);
+            }
+            else
+            {
+                if(minimum < abs(dq[i] - dq[i+1]))
+                   minimum = abs(dq[i] - dq[i+1]);
+            }
+        }
+ 
+ 
+        cout << '#' << test << ' ' << minimum << "\n";
+    }
+ 
+    return 0;
 }
