52d42cc325c72b811fd77db2f8b272405f871170
jm1224k <38150578+jm1224k@users.noreply.github.com>
2019-04-18 20:10:57 +0900

    Update 4307_ant_jeongmee.cpp

--- 2019/0412/4307_ant_jeongmee.cpp
+++ 2019/0412/4307_ant_jeongmee.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main() {
 	int t;
 	cin >> t;
-
 	while (t--) {
-		//모든 짧은 시간 중에 제일 긴 시간이 min 모든 긴 시간 중에 제일 긴 시간이 max!
 		int l, n, x;
 		cin >> l >> n;
 		int mn = -1, mx = -1;
-
 		while (n--) {	
 			cin >> x;
-			int max_x = (l - x) > x ? l - x : x;
-			int min_x = (l - x) < x ? l - x : x;
-			if (mx == -1 || mx < max_x) mx = max_x;
-			if (mn == -1 || mn < min_x) mn = min_x;
+			mn = max(mn, min(x, l-x));
+			mx = max(mx, max(x, l-x));	 
 		}
 		cout << mn << ' ' << mx << '\n';
 	}
