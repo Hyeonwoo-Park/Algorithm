382822f3356787776babcf7e5377aa2f79610f4d
jm1224k <38150578+jm1224k@users.noreply.github.com>
2019-05-10 14:52:28 +0900

    Update 7393_fandom_jeongmee.cpp

--- 2019/0504/7393_fandom_jeongmee.cpp
+++ 2019/0504/7393_fandom_jeongmee.cpp
@@ -1,18 +1,18 @@
 #include <cstdio>
 #include <cstring>
-//내가 푼 건 아님..ㅠㅠ
 
 const int mod = 1e9;
 int n, ans, dp[101][10][1025];
+//숫자가 사용됨 사용되지 않음을 비트로 표현. bitmask dp.
 
 int dfs(int len, int num, int msk) {
 
 	int &ret = dp[len][num][msk];
-	msk |= (1 << num);
+	msk |= (1 << num); //msk = msk||(1<<num); 원래 msk에 표시된 bit에서 내가 1로 만들고 싶은 비트도 1로 만들어줌!
 
 	if (num < 0 || num > 9) return 0;
-	if (len == n) return (msk == (1 << 10) - 1) ? 1 : 0;
-	if (ret != -1) return ret;
+	if (len == n) return (msk == (1 << 10) - 1) ? 1 : 0; //mak == 1111111111(2) ? 1 : 0;
+	if (ret != -1) return ret; // 값이 이미 있다.
 
 	return ret = ((long long)dfs(len + 1, num - 1, msk) + dfs(len + 1, num + 1, msk)) % mod;
 }
