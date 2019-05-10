8c50b8e174ab511512f7fbb3df4e0b197b38b3b0
jm1224k <38150578+jm1224k@users.noreply.github.com>
2019-05-10 14:44:54 +0900

    Update 7393_fandom_jeongmee.cpp

--- 2019/0504/7393_fandom_jeongmee.cpp
+++ 2019/0504/7393_fandom_jeongmee.cpp
@@ -7,7 +7,7 @@ int n, ans, dp[101][10][1025];
 
 int dfs(int len, int num, int msk) {
 
-  int &ret = dp[len][num][msk];
+	int &ret = dp[len][num][msk];
 	msk |= (1 << num);
 
 	if (num < 0 || num > 9) return 0;
@@ -19,18 +19,18 @@ int dfs(int len, int num, int msk) {
 
 
 int main() {
-	 int t;
-   scanf("%d",&t);
+	int t;
+   	scanf("%d",&t);
     
-   for(int tc = 1; tc <=t;tc++){
-      ans = 0;
-    	scanf("%d", &n);
+   	for(int tc = 1; tc <=t;tc++){
+      		ans = 0;
+    		scanf("%d", &n);
 	  	memset(dp, -1, sizeof(dp));
         
-		  for (int i = 1; i <= 9; i++)
-         ans = ((long long)ans + dfs(1, i, 0)) % mod;
+		for (int i = 1; i <= 9; i++)
+         		ans = ((long long)ans + dfs(1, i, 0)) % mod;
 
-		  printf("#%d %d\n", tc, ans);
+		printf("#%d %d\n", tc, ans);
    }
 	return 0;
 }
